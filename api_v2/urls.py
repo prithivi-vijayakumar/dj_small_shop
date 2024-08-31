ac27460cbad2b8092505a89c47841cc697693346
prithivi-vijayakumar <prithivirajan1207@gmail.com>
2024-08-31 19:35:30 +0530

    Cart Api v2 logic added

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from api_v2.views import CategoryListView, BrandListView, ProductListView, OrderListView
+from api_v2.views import CategoryListView, BrandListView, ProductListView, OrderListView, ClearCartView, CartItemView, \
+    CartView
 
 urlpatterns = [
     path('categories', CategoryListView.as_view(), name = 'category_list'),
     path('brands', BrandListView.as_view(), name='brand_list'),
     path('products', ProductListView.as_view(), name='product_list'),
-    path('order', OrderListView.as_view(), name='order_list')
+    path('order', OrderListView.as_view(), name='order_list'),
+
+    path('cart/', CartView.as_view(), name='cart'),
+    path('cart/item/', CartItemView.as_view(), name='cart-item'),
+    path('clear-cart', ClearCartView.as_view(), name='clear-cart'),
 ]
